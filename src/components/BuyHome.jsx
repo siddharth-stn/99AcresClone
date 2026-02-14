0418254aab27e148849ad93df314c62bbd0fe298
Siddharth Pande <sidarjunstn.sp@gmail.com>
2026-02-15 02:12:38 +0530

    BuyHome section updated

--- src/components/BuyHome.jsx
+++ src/components/BuyHome.jsx
@@ -15,6 +15,16 @@ const BuyHome = () => {
           <h2 className="font-bold text-[14px]">Explore all home buying options &rarr;</h2>
           <span className="text-[#8993A4] text-[12px]">Over 778,000 properties and 187,000 projects</span>
         </div>
+        <hr />
+        <div className="flex items-align my-10">
+          <figure className="w-10">
+            <img src="https://static.99acres.com/universalhp/img/BookRead.shared.png" alt="Book Read" />
+          </figure>
+          <div className="flex flex-col">
+            <h2 className="font-bold text-[16px]">Top Articles on home buying</h2>
+            <span className="text-[#8993A4] text-[12px] mt-0">Know more about the latest realty trends</span>
+          </div>
+        </div>
       </div>
     </div>
   );
